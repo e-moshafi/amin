cf98e45422ca751f6b7da147f0745d94fa87218b
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-16 22:48:26 +0330

    display setting update

--- inc/helpers/2.display.setting.helper.php
+++ inc/helpers/2.display.setting.helper.php
@@ -1,4 +1,18 @@
 <?php
-class AMINDisplaySettingHelper extends AMINSettingHelper{
-
+class AMINDisplaySettingHelper extends AMINSettingHelper
+{
+    private $option_key = 'display_settings_amin';
+    public function __construct()
+    {
+        $this->settings = $this->get_settings($this->option_key);
+    }
+    public function DisplayTopBar()
+    {
+        $option = $this->get_field('display_settings_amin_display_top_bar');
+        return $option == 'on';
+    }
+}
+function NEWAMINDisplaySetting()
+{
+    return new AMINDisplaySettingHelper();
 }
