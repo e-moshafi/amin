5bf27e30df7d68bcff5b8be81ca11d03a5077205
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-17 05:04:05 +0330

    update

--- template-parts/topbar/social.php
+++ template-parts/topbar/social.php
@@ -8,14 +8,17 @@
         <?php if (!empty($GenralSettingHelper->get_link_social('youtube'))) : ?>
             <li class="social-youtube"><a href="<?php echo $GenralSettingHelper->get_link_social('youtube') ?>"><i class="fab fa-youtube"></i></a></li>
         <?php endif; ?>
-        <?php if (!empty($GenralSettingHelper->get_link_social('twitter'))) : ?>
-            <li class="social-twitter"><a href="<?php echo $GenralSettingHelper->get_link_social('twitter') ?>"><i class="fab fa-twitter"></i></a></li>
+        <?php if (!empty($GenralSettingHelper->get_link_social('x'))) : ?>
+            <li class="social-twitter"><a href="<?php echo $GenralSettingHelper->get_link_social('x') ?>"><i class="fab fa-twitter"></i></a></li>
         <?php endif; ?>
         <?php if (!empty($GenralSettingHelper->get_link_social('whatsapp'))) : ?>
             <li class="social-whatsapp"><a href="<?php echo $GenralSettingHelper->get_link_social('whatsapp') ?>"><i class="fab fa-whatsapp"></i></a></li>
         <?php endif; ?>
         <?php if (!empty($GenralSettingHelper->get_link_social('instagram'))) : ?>
             <li class="social-instagram"><a href="<?php echo $GenralSettingHelper->get_link_social('instagram') ?>"><i class="fab fa-instagram"></i></a></li>
+        <?php endif; ?>
+        <?php if (!empty($GenralSettingHelper->get_link_social('telegram'))) : ?>
+            <li class="social-telegram"><a href="<?php echo $GenralSettingHelper->get_link_social('telegram') ?>"><i class="fab fa-telegram"></i></a></li>
         <?php endif; ?>
     </ul>
 </div>
