3ee0b9de21ff4204a83f496b4a11972b7650dbee
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-17 04:53:18 +0330

    update

--- inc/helpers/3.genral.setting.helper.php
+++ inc/helpers/3.genral.setting.helper.php
@@ -1,4 +1,12 @@
 <?php
 class AMINGenralSettingHelper extends AMINSettingHelper{
-    
+    private $option_key = 'general_amin';
+    public function __construct()
+    {
+        $this->settings = $this->get_settings($this->option_key);
+    }
+    public function get_link_social($social){
+        $option = $this->get_field($this->option_key . '_'.$social);
+        return $option;
+    }
 }
--- template-parts/topbar-section.php
+++ template-parts/topbar-section.php
@@ -15,19 +15,9 @@ $RightPosition = $AMINDisplaySetting->RightPositionTopBar();
                 ?>
             </div>
             <div class="col-md-6 d-none d-sm-block">
-                <div class="social-icons social-icons-colored-hover">
-                    <ul>
-                        <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="social-twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                        <li class="social-google"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                        <li class="social-pinterest"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                        <li class="social-vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>
-                        <li class="social-linkedin"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                        <li class="social-dribbble"><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                        <li class="social-youtube"><a href="#"><i class="fab fa-youtube"></i></a></li>
-                        <li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>
-                    </ul>
-                </div>
+                <?php
+                get_template_part('template-parts/topbar/social');
+                ?>
             </div>
         </div>
     </div>
