7ba5825b61db02f3f2d798fc73ab63945dafe7e8
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-17 04:39:30 +0330

    update

---
+++ template-parts/topbar/email_phone.php
@@ -0,0 +1,6 @@
+<ul class="top-menu">
+    <li><a href="#">درباره ما</a></li>
+    <li><a href="#">ویژگی ها</a></li>
+    <li><a href="#">پلن فروش</a></li>
+    <li><a href="#">مقررات</a></li>
+</ul>
