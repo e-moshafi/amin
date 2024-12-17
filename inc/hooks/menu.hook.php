25c7143df56bbc6c120e7f9b7e804362352a7a62
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-17 15:49:55 +0330

    header menu area

--- inc/hooks/menu.hook.php
+++ inc/hooks/menu.hook.php
@@ -3,7 +3,8 @@ function amin_theme_register_menus() {
     register_nav_menus(
         array(
             'topbar-menu' => __( 'Topbar Menu', 'amin' ),
-            'header-main-menu'  => __( 'Header Main Menu', 'amin' ),
+            'header-right-menu'  => __( 'Header Right Menu', 'amin' ),
+            'header-left-menu'  => __( 'Header Left Menu', 'amin' ),
         )
     );
 }
