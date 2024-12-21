23f4cbeee458fe542c15639dc9e9885b2f6fd342
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-21 22:41:26 +0330

    loop posts

--- template-parts/archive/items/loop.php
+++ template-parts/archive/items/loop.php
@@ -1,20 +1,27 @@
 <div class="post-item border">
     <div class="post-item-wrap">
         <div class="post-image">
-            <a href="#">
-                <img alt="" src="images/blog/12.jpg">
-            </a>
-            <span class="post-meta-category"><a href="index.html">سبک زندگی</a></span>
+            <?php if (has_post_thumbnail()): ?>
+                <a href="<?php the_permalink() ?>">
+                    <img alt="" src="<?php echo get_the_post_thumbnail_url() ?>">
+                </a>
+            <?php endif; ?>
+            <?php
+            $categories = get_the_category();
+            if (!empty($categories)):
+                foreach ($categories as $category):
+            ?>
+                    <span class="post-meta-category"><a href="<?php echo  esc_url( get_category_link( $category->term_id )) ?>"><?php echo esc_html( $category->name ) ?></a></span>
+            <?php
+                endforeach;
+            endif; ?>
         </div>
         <div class="post-item-description">
-            <span class="post-meta-date"><i class="fa fa-calendar-o"></i>21 بهمن 1400</span>
-            <span class="post-meta-comments"><a href="index.html"><i class="fa fa-comments-o"></i>33
-                    دیدگاه</a></span>
-            <h2><a href="#">پست استاندارد با یک تصویر
-                </a></h2>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم، لورم ایپسوم متن ساختگی با تولید سادگی
-                نامفهوم، لورم ایپسوم.</p>
-            <a href="#" class="item-link">بیشتر بخوانید <i class="icon-chevron-left"></i></a>
+            <span class="post-meta-date"><i class="fa fa-calendar-o"></i><?php echo jdate('d F Y', strtotime(get_the_date('Y-m-d h:i:s'))) ?></span>
+            <span class="post-meta-comments"><a href="index.html"><i class="fa fa-comments-o"></i><?php echo __('comment','amin').' '.get_comments_number() ?></a></span>
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <?php the_excerpt() ?>
+            <a href="<?php the_permalink() ?>" class="item-link"><?php echo __('read more', 'amin') ?><i class="icon-chevron-left"></i></a>
         </div>
     </div>
 </div>
