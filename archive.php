51d489ecc7ca1fe6b202b50cb09cfb2ce357d9ec
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-24 21:16:16 +0330

    update archive

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php get_header() ?>
+<?php
+$DisplaySetting = NEWAMINDisplaySetting();
+?>
+<section id="page-content">
+
+    <div class="container">
+        <div class="row">
+            <?php
+            if ($DisplaySetting->PostsSidebarPositionAllow('right')):
+            ?>
+                <?php get_sidebar('right'); ?>
+            <?php endif; ?>
+            <div class="content col-lg">
+                <?php echo get_template_part('template-parts/archive') ?>
+            </div>
+            <?php
+            if ($DisplaySetting->PostsSidebarPositionAllow('left')):
+            ?>
+                <?php get_sidebar('left'); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+
+</section>
+<?php
+get_footer();
