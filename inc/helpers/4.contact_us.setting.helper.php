c6c2f0aa71dbaebcf72cb7aad9e2c4693193bae1
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 12:32:52 +0330

    contact us heleper

---
+++ inc/helpers/4.contact_us.setting.helper.php
@@ -0,0 +1,29 @@
+<?php
+class AMINContactUsHelper extends AMINSettingHelper
+{
+    private $option_key = 'contact_us_settings_amin';
+    public function __construct()
+    {
+        $this->settings = $this->get_settings($this->option_key);
+    }
+    public function GetAddress(){
+        $option = $this->get_field($this->option_key . '_address', 3);
+        return $option;
+    }
+    public function GetPhone(){
+        $option = $this->get_field($this->option_key . '_phone', 3);
+        return $option;
+    }
+    public function GetEmail(){
+        $option = $this->get_field($this->option_key . '_email', 3);
+        return $option;
+    }
+    public function GetWorkingTime(){
+        $option = $this->get_field($this->option_key . '_working_time', 3);
+        return $option;
+    }
+}
+function NEWAMINContactUs()
+{
+    return new AMINContactUsHelper();
+}
