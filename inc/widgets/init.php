c7c5803c71beae2b266c6027a6ece123c6e57df4
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 09:42:22 +0330

    add list

--- inc/widgets/init.php
+++ inc/widgets/init.php
@@ -4,6 +4,7 @@ require_once "search.php";
 require_once "contact-information.php";
 require_once "carousel-post.php";
 require_once "last-posts.php";
+require_once "list.php";
 function register_amin_wp_widget() {
     register_widget( 'AminTagsWpWidgets' );
     register_widget( 'AminSearchWpWidgets' );
---
+++ inc/widgets/list.php
@@ -0,0 +1,47 @@
+<?php
+class AminListWpWidgets extends WP_Widget
+{
+
+    // سازنده ابزارک
+    public function __construct()
+    {
+        parent::__construct(
+            'amin_wp_list_widget', // شناسه ابزارک
+            __('List Amin', 'amin'), // نام ابزارک
+            array('description' => __('', 'amin')) // توضیحات ابزارک
+        );
+    }
+
+    // نمایش ابزارک در صفحه
+    public function widget($args, $instance)
+    {
+        echo $args['before_widget'];
+        if (! empty($instance['title'])) {
+            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        }
+        get_search_form();
+        echo $args['after_widget'];
+    }
+
+    // فرم تنظیمات ابزارک در پیشخوان
+    public function form($instance)
+    {
+        $title = ! empty($instance['title']) ? $instance['title'] : __('Search','amin');
+    ?>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php _e(esc_attr('Title:','amin')); ?></label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+
+<?php
+    }
+
+    // به‌روزرسانی تنظیمات ابزارک
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = (! empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+}
+?>
