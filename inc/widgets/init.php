e683f8ce48957ae8f0d0a91fd7e9b738cfae6c9f
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 06:51:27 +0330

    add search form

--- inc/widgets/init.php
+++ inc/widgets/init.php
@@ -1,6 +1,9 @@
 <?php 
 require_once "tags.php";
+require_once "search.php";
 function register_amin_wp_widget() {
     register_widget( 'AminTagsWpWidgets' );
+    register_widget( 'AminSearchWpWidgets' );
+
 }
 add_action( 'widgets_init', 'register_amin_wp_widget' );
