d53d596d9d7a9a878323d2946a324fcacc9202ab
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 05:18:12 +0330

    update

--- template-parts/archive.php
+++ template-parts/archive.php
@@ -10,7 +10,27 @@ global $wp_query;
             get_template_part('template-parts/archive/items/loop');
         endwhile;
     ?>
+    <?php
+    else:
+        echo '<p>هیچ پستی یافت نشد.</p>';
+    endif;
+    wp_reset_postdata();
+    ?>
 </div>
 <?php
-    else:
-    endif;
+
+if ($wp_query->have_posts()):
+    $html_pagination = get_the_posts_pagination(array('mid_size' => 2, 'class' => '', 'screen_reader_text' => '', 'type' => 'list', 'prev_text' => __('«'), 'next_text' => __('»'),));
+    $html_pagination = str_replace('<ul class=\'page-numbers\'>', '<ul class="pagination">', $html_pagination);
+    $html_pagination = str_replace('<li>', '<li class="page-item">', $html_pagination);
+    $html_pagination = str_replace('page-numbers', 'page-link', $html_pagination);
+    $pattern = '/<li class="page-item"><span aria-current="page" class="page-link current">(.*?)<\/span><\/li>/s';
+    preg_match($pattern, $html_pagination, $matches);
+    if(!empty($matches)){
+        $active_page_before=$matches[0];
+        $active_page = str_replace('page-item', 'page-item active', $active_page_before);
+        $html_pagination=str_replace($active_page_before, $active_page, $html_pagination);
+    }
+    echo $html_pagination;
+endif;
+?>
