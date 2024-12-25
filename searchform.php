7bc7918af541057a9e8063bc3540ad66a47fd91d
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 06:44:54 +0330

    update

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form id="widget-search-form-sidebar" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
     <div class="input-group">
         <input type="text" aria-required="true" name="s"
-            class="form-control widget-search-form" value="<?php echo get_search_query(); ?>" placeholder="جستجو کنید ...">
+            class="form-control widget-search-form" value="<?php echo get_search_query(); ?>" placeholder="<?php __('search ...','amin') ?>">
         <div class="input-group-append">
             <button class="btn" type="submit"><i class="fa fa-search"></i></button>
         </div>
