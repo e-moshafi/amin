457c77149ec2d927c71f072245c5873031f1e95e
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2024-12-25 12:20:40 +0330

    add map

--- footer.php
+++ footer.php
@@ -18,7 +18,10 @@
 
 <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
 <?php wp_footer(); ?>
-
+<script type='text/javascript'
+        src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDKXKdHQdtqgPVl2HI2RnUa_1bjCxRCQo4&callback=initMap'></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/template/plugins/gmap3/gmap3.min.js" ?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri() ."/assets/template/plugins/gmap3/map-styles.js" ?>"></script>
 </body>
 
 </html>
