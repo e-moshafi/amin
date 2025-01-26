ef83951776a3681851a412f0dd64804dc67d92f3
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-01-27 01:18:39 +0330

    footer

--- footer.php
+++ footer.php
@@ -1,16 +1,10 @@
 <?php
-
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package storefront
- */
-
+if (function_exists('elementor_theme_do_location') && elementor_theme_do_location('footer')) :
+    elementor_theme_do_location('footer');
+else:
+    get_template_part('template-parts/footer', 'section');
+endif;
 ?>
-<?php get_template_part('template-parts/footer', 'section') ?>
-
 
 
 </div>
@@ -19,8 +13,8 @@
 <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
 <?php wp_footer(); ?>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=<?php echo NEWAMINContactUsSetting()->GetGoogleMapApi() ?>&callback=initMap"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/template/plugins/gmap3/gmap3.min.js" ?>"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri() ."/assets/template/plugins/gmap3/map-styles.js" ?>"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/template/plugins/gmap3/gmap3.min.js" ?>"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() . "/assets/template/plugins/gmap3/map-styles.js" ?>"></script>
 </body>
 
 </html>
