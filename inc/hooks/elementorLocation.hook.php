cd749c963e8c278fcd454c45f354ab90cf0e903d
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-01-27 01:05:57 +0330

    update

--- inc/hooks/elementorLocation.hook.php
+++ inc/hooks/elementorLocation.hook.php
@@ -1,6 +1,6 @@
 <?php
-function register_elementor_locations( $elementor_theme_manager ) {
+function register_elementor_locations_amin( $elementor_theme_manager ) {
     $elementor_theme_manager->register_location( 'header' );
     $elementor_theme_manager->register_location( 'footer' );
 }
-add_action( 'elementor/theme/register_locations', 'register_elementor_locations' );
+add_action( 'elementor/theme/register_locations', 'register_elementor_locations_amin' );
