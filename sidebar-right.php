a458107e9f30b2a25d285ba5dd75e676d0bb0b14
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-04-21 22:08:17 +0330

    sidebar elementor

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,2 +1,6 @@
 <?php
-get_template_part('template-parts/sidebar','section',['position'=>'right']);
+if (function_exists('elementor_theme_do_location') && elementor_theme_do_location('header')) :
+    elementor_theme_do_location('sidebar_right');
+else:
+    get_template_part('template-parts/sidebar', 'section', ['position' => 'right']);
+endif;
--- sidebar.php
+++ sidebar.php
@@ -1,2 +1,6 @@
 <?php
-get_template_part('template-parts/sidebar','section',['position'=>'left']);
+if (function_exists('elementor_theme_do_location') && elementor_theme_do_location('header')) :
+    elementor_theme_do_location('sidebar_left');
+else:
+    get_template_part('template-parts/sidebar', 'section', ['position' => 'left']);
+endif;
