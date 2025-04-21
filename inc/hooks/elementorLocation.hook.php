a2094e93da3ccc72c81cd6d75367ffab4cca795d
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-04-21 22:11:15 +0330

    add section elemntor

--- inc/hooks/elementorLocation.hook.php
+++ inc/hooks/elementorLocation.hook.php
@@ -1,7 +1,21 @@
 <?php
-function register_elementor_locations_amin( $elementor_theme_manager ) {
-    $elementor_theme_manager->register_location( 'header' );
-    $elementor_theme_manager->register_location( 'footer' );
-    $elementor_theme_manager->register_location( 'sidebar_left' );
+function register_elementor_locations_amin($elementor_theme_manager)
+{
+    $elementor_theme_manager->register_location('header', [
+        'label' => __('Header'),
+        'multiple' => true
+    ]);
+    $elementor_theme_manager->register_location('footer', [
+        'label' => __('Footer'),
+        'multiple' => true
+    ]);
+    $elementor_theme_manager->register_location('sidebar_left', [
+        'label' => __('Sidebar left', 'amin'),
+        'multiple' => true
+    ]);
+    $elementor_theme_manager->register_location('sidebar_right', [
+        'label' => __('Sidebar Right', 'amin'),
+        'multiple' => true
+    ]);
 }
-add_action( 'elementor/theme/register_locations', 'register_elementor_locations_amin' );
+add_action('elementor/theme/register_locations', 'register_elementor_locations_amin');
