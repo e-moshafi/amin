f0112ffc487e8443cd14930902b78f0b1d476fd2
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-04-21 20:36:46 +0330

    add update

--- inc/hooks/elementorLocation.hook.php
+++ inc/hooks/elementorLocation.hook.php
@@ -2,5 +2,6 @@
 function register_elementor_locations_amin( $elementor_theme_manager ) {
     $elementor_theme_manager->register_location( 'header' );
     $elementor_theme_manager->register_location( 'footer' );
+    $elementor_theme_manager->register_location( 'sidebar_left' );
 }
 add_action( 'elementor/theme/register_locations', 'register_elementor_locations_amin' );
