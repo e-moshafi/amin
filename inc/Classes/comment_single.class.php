05db8f474c35aee735f87e37e9e29e5137802324
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-04-21 21:38:07 +0330

    fixed comment walker

--- inc/Classes/comment_single.class.php
+++ inc/Classes/comment_single.class.php
@@ -5,21 +5,22 @@ class AminCoustomComment extends Walker_Comment
     {
 ?>
 
-        <?php if ('0' == $comment->comment_approved) : ?>
-            <em><?php _e('دیدگاه شما در انتظار تایید است.'); ?></em><br>
-        <?php endif; ?>
-        <div <?php comment_class('comment'); ?> id="comment-<?php comment_ID(); ?>">
-            <div class="image"> <?php echo get_avatar($comment, 48); ?> </div>
-            <div class="text">
-                <h5 class="name"> <?php echo get_comment_author() ?></h5>
-                <span class="comment_date"> <?php printf(__('%1$s در %2$s'), jdate('m F Y', strtotime(get_comment_date('Y-m-d'))), get_comment_time()); ?> </span>
-                <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
-                <div class="text_holder">
-                    <?php comment_text(); ?>
+        <div <?php comment_class($this->has_children ? 'parent' : '', $comment); ?> id="csomment-<?php comment_ID(); ?>">
+            <?php if ('0' == $comment->comment_approved) : ?>
+                <em><?php _e('دیدگاه شما در انتظار تایید است.'); ?></em><br>
+            <?php endif; ?>
+            <div <?php comment_class('comment'); ?> id="comment-<?php comment_ID(); ?>">
+                <div class="image"> <?php echo get_avatar($comment, 48); ?> </div>
+                <div class="text">
+                    <h5 class="name"> <?php echo get_comment_author() ?></h5>
+                    <span class="comment_date"> <?php printf(__('%1$s در %2$s'), jdate('m F Y', strtotime(get_comment_date('Y-m-d'))), get_comment_time()); ?> </span>
+                    <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
+                    <div class="text_holder">
+                        <?php comment_text(); ?>
+                    </div>
                 </div>
             </div>
-        </div>
-<?php
+    <?php
     }
 }
-?>
+    ?>
