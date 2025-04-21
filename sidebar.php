068f2d0d59554c0f7de07684b5389c62d28cb5a9
erfan.moshafi2018@gmail.com <erfan.moshafi2018@gmail.com>
2025-04-21 22:18:31 +0330

    update sidebar Compatibility with Element

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,2 @@
 <?php
-if (function_exists('elementor_theme_do_location') && elementor_theme_do_location('header')) :
-    elementor_theme_do_location('sidebar_left');
-else:
-    get_template_part('template-parts/sidebar', 'section', ['position' => 'left']);
-endif;
+get_template_part('template-parts/sidebar', 'section', ['position' => 'left']);
--- template-parts/sidebar-section.php
+++ template-parts/sidebar-section.php
@@ -1,8 +1,11 @@
 <div class="sidebar <?php if (NEWAMINDisplaySetting()->StickySidebar()) echo "sticky-sidebar";  ?>  col-lg-3">
     <?php
     $position = !empty($args['position']) ? $args['position'] : 'left';
-    ?>
-    <?php if (is_active_sidebar($position . '-sidebar')) : ?>
-        <?php dynamic_sidebar($position . '-sidebar'); ?>
-    <?php endif; ?>
+    if (function_exists('elementor_theme_do_location') && elementor_theme_do_location('sidebar_'.$position)) :
+        elementor_theme_do_location('sidebar_'.$position);
+    else:
+        if (is_active_sidebar($position . '-sidebar')) : ?>
+            <?php dynamic_sidebar($position . '-sidebar'); ?>
+    <?php endif;
+    endif; ?>
 </div>
